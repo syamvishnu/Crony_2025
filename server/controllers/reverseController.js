ec1151c7099b7ea727d9720ccfdfa5a0aca8cf3f
syam <syamvishnu@gmail.com>
2024-11-12 10:03:48 +0530

    Keyword search both end complete

--- server/controllers/reverseController.js
+++ server/controllers/reverseController.js
@@ -1,49 +1,86 @@
-import sdrModel from "../model/sdrModel.js"
+import sdrModel from "../model/sdrModel.js";
+
+// Assuming you have a separate initialization script or database migration:
 
 const reverseSearch = async (req, res, next) => {
-    console.log(req.body)
   const { key2, key1, father1, email1 } = req.body;
 
-  
+  console.log("Search criteria:", { key2, key1, father1, email1 });
 
-  if (!key2 && key1) {
-    const data1 = await sdrModel
-      .find({
-        $and: [{ localaddress: { $regex: new RegExp(key1, "i") } }],
-      })
-      .select("subscribername localaddress tnumber addressproff dob")
-      .lean()
-      .limit(100);
+  // Build the query object dynamically
+  const query = {};
+  if (key2) query.subscribername = { $regex: `.*${key2}.*`, $options: "i" };
+  if (key1) query.localaddress = { $regex: `.*${key1}.*`, $options: "i" };
+  if (father1)
+    query.fatherhusname = { $regex: `.*${father1}.*`, $options: "i" };
+  if (email1) query.email = { $regex: `.*${email1}.*`, $options: "i" };
 
-    return res.json({
-      status: 200,
-      message: "Data Found",
-      data1,
-    });
-  } else {
+  try {
     const data1 = await sdrModel
-      .find({
-        $and: [
-          { subscribername: { $regex: new RegExp(key2, "i") } },
-          { localaddress: { $regex: new RegExp(key1, "i") } },
-          { fatherhusname: { $regex: new RegExp(father1, "i") } },
-          { email: { $regex: new RegExp(email1, "i") } },
-        ],
-      })
+      .find(query)
       .select(
-        "subscribername localaddress tnumber addressproff dob alternative email"
+        "subscribername localaddress tnumber addressproff dob alternative email fatherhusname"
       )
       .lean()
       .limit(100);
 
+    console.log("Data found:", data1);
+
+    if (data1.length === 0) {
+      console.log("No data found matching the criteria.");
+    }
+
     return res.json({
       status: 200,
-      message: "Data Found",
+      message: data1.length > 0 ? "Data Found" : "No Data Found",
       data1,
     });
+  } catch (error) {
+    console.error("Error occurred in reverseSearch:", error);
+    return res.status(500).json({
+      status: 500,
+      message: "An error occurred while searching for data",
+      error: error.message,
+    });
   }
-
 };
+
+// if (!key2 && key1) {
+//   const data1 = await sdrModel
+//     .find({
+//       $and: [{ localaddress: { $regex: new RegExp(key1, "i") } }],
+//     })
+//     .select("subscribername localaddress tnumber addressproff dob")
+//     .lean()
+//     .limit(100);
+
+//   return res.json({
+//     status: 200,
+//     message: "Data Found",
+//     data1,
+//   });
+// } else {
+//   const data1 = await sdrModel
+// .find({
+//   $and: [
+//     { subscribername: { $regex: new RegExp(key2, "i") } },
+//     { localaddress: { $regex: new RegExp(key1, "i") } },
+//     { fatherhusname: { $regex: new RegExp(father1, "i") } },
+//     { email: { $regex: new RegExp(email1, "i") } },
+//   ],
+// })
+// .select(
+//   "subscribername localaddress tnumber addressproff dob alternative email"
+// )
+// .lean()
+// .limit(100);
+
+// return res.json({
+//   status: 200,
+//   message: "Data Found",
+//   data1,
+// });
+// }
 
 const mutliSearch = async (req, res) => {
   const { name, father, address, email } = req.body;
--- server/controllers/sdrController.js
+++ server/controllers/sdrController.js
@@ -1,59 +1,122 @@
+// import sdrModel from "../model/sdrModel.js";
+
+// const sdrSearch = async (req, res, next) => {
+//   const { data, option, userData } = req.body;
+//   const Num = Number(data);
+
+//   if (option == "Number") {
+//     if (!Num) {
+//       return res.status(400).json("Please Enter Number");
+//     }
+
+//     if (Num.toString().length != 10) {
+//       return res.status(400).json("Please check the number");
+//     }
+
+//     console.log(Num);
+
+//     const numberFound = await sdrModel.findOne({ tnumber: Num }).lean();
+
+//     if (numberFound) {
+//       return res.status(200).json({
+//         numberFound,
+//       });
+//     } else {
+//       return res.status(400).json("No Data Found");
+//     }
+//   }
+
+//   if (option == "ID") {
+//     if (!Num) {
+//       return res.status(400).json("Please Enter Input");
+//     }
+
+//     const numberFound = await sdrModel
+//       .find({
+//         $or: [
+//           { addressproff: { $regex: data, $options: "i" } },
+//           { aadhaar: Num },
+//         ],
+//       })
+//       .select(
+//         "tnumber subscribername fatherhusname aadhaar addressproff alternative dob localaddress service_provider"
+//       )
+//       .lean()
+//       .limit(100);
+
+//     if (numberFound && numberFound.length > 0) {
+//       // Check if numberFound is not empty
+//       return res.status(200).json({
+//         numberFound,
+//       });
+//     } else {
+//       return res.status(400).json({
+//         message: "No Data Found",
+//       });
+//     }
+//   }
+// };
+
+// export default sdrSearch;
+
 import sdrModel from "../model/sdrModel.js";
 
 const sdrSearch = async (req, res, next) => {
   const { data, option, userData } = req.body;
   const Num = Number(data);
 
-  if (option == "Number") {
-    if (!Num) {
-      return res.status(400).json("Please Enter Number");
+  try {
+    if (option === "Number") {
+      if (!Num) {
+        return res.status(400).json("Please Enter Number");
+      }
+
+      if (Num.toString().length !== 10) {
+        return res.status(400).json("Please check the number");
+      }
+
+      console.log(Num);
+
+      const numberFound = await sdrModel.findOne({ tnumber: Num }).lean();
+
+      if (numberFound) {
+        return res.status(200).json({ numberFound });
+      } else {
+        return res.status(400).json("No Data Found");
+      }
     }
 
-    if (Num.toString().length != 10) {
-      return res.status(400).json("Please check the number");
+    if (option === "ID") {
+      if (!Num) {
+        return res.status(400).json("Please Enter Input");
+      }
+
+      const numberFound = await sdrModel
+        .find({
+          $or: [
+            { addressproff: { $regex: data, $options: "i" } },
+            { aadhaar: Num },
+          ],
+        })
+        .select(
+          "tnumber subscribername fatherhusname aadhaar addressproff alternative dob localaddress service_provider"
+        )
+        .lean()
+        .limit(100);
+
+      if (numberFound && numberFound.length > 0) {
+        return res.status(200).json({ numberFound });
+      } else {
+        return res.status(400).json({ message: "No Data Found" });
+      }
     }
-
-    console.log(Num);
-
-    const numberFound = await sdrModel.findOne({ tnumber: Num }).lean();
-
-    if (numberFound) {
-      return res.status(200).json({
-        numberFound,
-      });
-    } else {
-      return res.status(400).json("No Data Found");
-    }
-  }
-
-  if (option == "ID") {
-    if (!Num) {
-      return res.status(400).json("Please Enter Input");
-    }
-
-    const numberFound = await sdrModel
-      .find({
-        $or: [
-          { addressproff: { $regex: data, $options: "i" } },
-          { aadhaar: Num },
-        ],
-      })
-      .select(
-        "tnumber subscribername fatherhusname aadhaar addressproff alternative dob localaddress service_provider"
-      )
-      .lean()
-      .limit(100);
-
-    if (numberFound && numberFound.length > 0) {
-      // Check if numberFound is not empty
-      return res.status(200).json({
-        numberFound,
-      });
-    } else {
-      return res.status(400).json({
-        message: "No Data Found",
-      });
-    }
+  } catch (error) {
+    console.error("Error occurred in sdrSearch:", error);
+    return res.status(500).json({
+      status: 500,
+      message: "An error occurred while searching for data",
+      error: error.message,
+    });
   }
 };
 
